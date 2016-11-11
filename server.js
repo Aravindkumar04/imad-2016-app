e41947bfb9260d3fba227148f32b83bec79e38da
Aravindkumar04 <aravindkumar.92apr@gmail.com>
2016-11-12 01:36:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/path', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'path.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
